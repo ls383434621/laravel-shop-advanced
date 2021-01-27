fa676217bb2a422b889d4b9c371a168fd962fcb7
vagrant <vagrant@homestead>
2021-01-27 09:38:56 +0000

    使用 Elasticsearch 重构商品搜索功能

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -8,10 +8,100 @@ use App\Models\Product;
 use App\Models\Category;
 use App\Services\CategoryService;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ProductsController extends Controller
-{
-    public function index(Request $request,CategoryService $categoryService)
+{	
+	public function index(Request $request)
+    {
+        $page    = $request->input('page', 1);
+        $perPage = 16;
+
+        // 构建查询
+        $params = [
+            'index' => 'products',
+            'body'  => [
+                'from'  => ($page - 1) * $perPage, // 通过当前页数与每页数量计算偏移值
+                'size'  => $perPage,
+                'query' => [
+                    'bool' => [
+                        'filter' => [
+                            ['term' => ['on_sale' => true]],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+		if ($search = $request->input('search', '')) {
+			$keywords = array_filter(explode(' ', $search));
+			$params['body']['query']['bool']['must'] = [];
+			foreach ($keywords as $keyword) {
+				$params['body']['query']['bool']['must'][] = [
+					[
+						'multi_match' => [
+							'query'  => $keyword,
+							'fields' => [
+								'title^3',
+								'long_title^2',
+								'category^2', // 类目名称
+								'description',
+								'skus_title',
+								'skus_description',
+								'properties_value',
+							],
+						],
+					]
+				];
+			}	
+        }
+		if ($request->input('category_id') && $category = Category::find($request->input('category_id'))) {
+            if ($category->is_directory) {
+                // 如果是一个父类目，则使用 category_path 来筛选
+                $params['body']['query']['bool']['filter'][] = [
+                    'prefix' => ['category_path' => $category->path.$category->id.'-'],
+                ];
+            } else {
+                // 否则直接通过 category_id 筛选
+                $params['body']['query']['bool']['filter'][] = ['term' => ['category_id' => $category->id]];
+            }
+        }
+        // 是否有提交 order 参数，如果有就赋值给 $order 变量
+        // order 参数用来控制商品的排序规则
+        if ($order = $request->input('order', '')) {
+            // 是否是以 _asc 或者 _desc 结尾
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                // 如果字符串的开头是这 3 个字符串之一，说明是一个合法的排序值
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    // 根据传入的排序值来构造排序参数
+                    $params['body']['sort'] = [[$m[1] => $m[2]]];
+                }
+            }
+        }
+
+        $result = app('es')->search($params);
+
+        // 通过 collect 函数将返回结果转为集合，并通过集合的 pluck 方法取到返回的商品 ID 数组
+        $productIds = collect($result['hits']['hits'])->pluck('_id')->all();
+        // 通过 whereIn 方法从数据库中读取商品数据
+        $products = Product::query()
+            ->whereIn('id', $productIds)
+			->orderByRaw(sprintf("FIND_IN_SET(id, '%s')", join(',', $productIds)))
+            ->get();
+        // 返回一个 LengthAwarePaginator 对象
+        $pager = new LengthAwarePaginator($products, $result['hits']['total']['value'], $perPage, $page, [
+            'path' => route('products.index', false), // 手动构建分页的 url
+        ]);
+
+        return view('products.index', [
+            'products' => $pager,
+            'filters'  => [
+                'search' => $search,
+                'order'  => $order,
+            ],
+			'category' => $category ?? null,
+        ]);
+    }
+    public function index1(Request $request,CategoryService $categoryService)
     {
         // 创建一个查询构造器
         $builder = Product::query()->where('on_sale', true);
